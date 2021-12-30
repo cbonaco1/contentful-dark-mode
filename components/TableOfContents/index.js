8351ce4c5037a3a1407700efc6757722a425b7f1
cbonaco1 <christian.bonacore@gmail.com>
2021-12-29 20:31:10 -0500

    table of content styles

--- components/TableOfContents/index.js
+++ components/TableOfContents/index.js
@@ -1,21 +1,24 @@
 import Link from "next/link";
+import styles from "./TableOfContents.module.scss";
 
 const TableOfContents = ({links}) => {
   return (
-    <div>
-      <p>Table of Contents</p>
-      <ul>
-        {
-          links.map((link) => {
-            const { href, title, id } = link;
-            return (
-              <li key={id}>
-                <Link href={href}>{title}</Link>
-              </li>
-            )
-          })
-        }
-      </ul>
+    <div className={styles.container}>
+      <p className={styles.header}>Table of Contents</p>
+      <div className={styles.content}>
+        <ul>
+          {
+            links.map((link, index) => {
+              const { href, title, id } = link;
+              return (
+                <li className={`${index === 0 ? styles.active : ''} ${styles.listItem}`} key={id}>
+                  <Link href={href}>{title}</Link>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
     </div>
   )
 }
