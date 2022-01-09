3ebd80976008fd1de0410fc53018b524466a3075
cbonaco1 <christian.bonacore@gmail.com>
2022-01-09 15:16:42 -0500

    render lesson copy markdown

--- layouts/Layout.js
+++ layouts/Layout.js
@@ -4,7 +4,7 @@ import styles from "./Layout.module.scss";
 const Layout = ({ children, lessonLinks }) => {
   return (
     <div className={styles.layout}>
-      <div>
+      <div className={styles.tocWrapper}>
         <TableOfContents links={lessonLinks} />
       </div>
       <div className={styles.content}>
--- pages/courses/[course]/lessons/[lesson].js
+++ pages/courses/[course]/lessons/[lesson].js
@@ -1,19 +1,22 @@
 import { createClient } from 'contentful'
+import { richTextFromMarkdown } from "@contentful/rich-text-from-markdown";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import Head from 'next/head'
 import { Fragment } from "react";
 import Layout from "layouts/Layout";
+import styles from "./Lesson.module.scss";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
 })
 
-const Lesson = ({ lesson, course, nextLessonUrl }) => {
+const Lesson = ({ lesson, course, nextLessonUrl, sections }) => {
   const { query } = useRouter();
   const { lessons, slug, title:courseTitle } = course;
-  const { title:lessonTitle} = lesson;
+  const { title:lessonTitle, modules } = lesson;
 
   const lessonLinks = lessons.map((lesson) => {
     return {
@@ -35,13 +38,18 @@ const Lesson = ({ lesson, course, nextLessonUrl }) => {
         <title>{courseTitle} | {lessonTitle}</title>
       </Head>
       <Layout lessonLinks={lessonLinks}>
-        <h1>{lessonTitle}</h1>
+        <h1 className={styles.title}>{lessonTitle}</h1>
+        <div className={styles.content}>
+          {sections && sections.map((section) => {
+            return documentToReactComponents(section)
+          })}
+        </div>
+        {(nextLessonUrl && (
+          <Link href={nextLessonUrl}>
+            <a className="button">Go to the next lesson</a>
+          </Link>
+        ))}
       </Layout>
-      {(nextLessonUrl && (
-        <Link href={nextLessonUrl}>
-          <a className="button">Go to the next lesson</a>
-        </Link>
-      ))}
     </Fragment>
   )
 }
@@ -88,6 +96,7 @@ export async function getStaticProps(context) {
   })
 
   const courseObj = course.items[0].fields
+  const lessonObj = lesson.items[0].fields;
 
   const indexOfCourseLesson = courseObj.lessons.findIndex((lesson) => {
     return lesson.fields.slug === params.lesson;
@@ -99,10 +108,21 @@ export async function getStaticProps(context) {
     nextLessonUrl = `/courses/${params.course}/lessons/${nextLesson.fields.slug}`;
   }
 
+  const copySections = [];
+  lessonObj.modules.forEach(async (module) => {
+    if (module.sys.contentType.sys.id === 'lessonCopy') {
+      copySections.push(module.fields.copy);
+    }
+  })
+
+  const conversions = copySections.map(async (copy) => await richTextFromMarkdown(copy) );
+  const sections = await Promise.all(conversions);
+
   return {
     props: {
       nextLessonUrl,
-      lesson: lesson.items[0].fields,
+      sections,
+      lesson: lessonObj,
       course: courseObj,
     }
   }
